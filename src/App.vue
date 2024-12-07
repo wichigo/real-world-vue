b9e743fdd9c48b10cc10d71dc5c09c8e0aaac145
wichigo <wichigofr@gmail.com>
2024-12-07 21:48:32 +0100

    'undo'

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="wrapper">
         <nav>
           <RouterLink :to="{ name: 'event-list' }">Events</RouterLink> |
-          <RouterLink :to="{ name: 'about' }">Aboutazzr</RouterLink>
+          <RouterLink :to="{ name: 'about' }">About</RouterLink>
         </nav>
       </div>
     </header>
